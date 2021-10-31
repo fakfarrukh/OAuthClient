f0de1d30fc6e21356c27a8565077faade18b7569
Farrukh Aziz <fak_farrukh@yahoo.com>
2021-10-31 16:07:20 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace OAuthClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txturl = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtToken = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtResponse = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL:";
+            // 
+            // txturl
+            // 
+            this.txturl.Location = new System.Drawing.Point(104, 7);
+            this.txturl.Margin = new System.Windows.Forms.Padding(4);
+            this.txturl.Name = "txturl";
+            this.txturl.Size = new System.Drawing.Size(345, 22);
+            this.txturl.TabIndex = 1;
+            this.txturl.Text = "https://localhost:44362";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(104, 46);
+            this.txtUser.Margin = new System.Windows.Forms.Padding(4);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(345, 22);
+            this.txtUser.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 49);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "UserName:";
+            // 
+            // txtToken
+            // 
+            this.txtToken.Location = new System.Drawing.Point(104, 81);
+            this.txtToken.Margin = new System.Windows.Forms.Padding(4);
+            this.txtToken.Multiline = true;
+            this.txtToken.Name = "txtToken";
+            this.txtToken.ReadOnly = true;
+            this.txtToken.Size = new System.Drawing.Size(345, 93);
+            this.txtToken.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 85);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Token:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(332, 203);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 28);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Request";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtResponse
+            // 
+            this.txtResponse.Location = new System.Drawing.Point(20, 239);
+            this.txtResponse.Margin = new System.Windows.Forms.Padding(4);
+            this.txtResponse.Multiline = true;
+            this.txtResponse.Name = "txtResponse";
+            this.txtResponse.ReadOnly = true;
+            this.txtResponse.Size = new System.Drawing.Size(429, 136);
+            this.txtResponse.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 390);
+            this.Controls.Add(this.txtResponse);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtToken);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txturl);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txturl;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtToken;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtResponse;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OAuthClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string oAuthInfo = GetAuthorizeToken().Result;
+
+                // Process response access token info.  
+
+                // Call REST Web API method with authorize access token.  
+                GetInfo(oAuthInfo);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+            
+        }
+
+        public async Task<string> GetAuthorizeToken()
+        {
+            // Initialization.  
+            string responseObj = string.Empty;
+
+            // Posting.  
+
+            string user = "admin";
+            var client = new RestClient(txturl.Text+"/Token");
+            var request = new RestRequest(Method.POST);
+            user = txtUser.Text;
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("content-type", "application/x-www-form-urlencoded");
+            request.AddParameter("application/x-www-form-urlencoded", "grant_type=password&username="+user+"&password=admin", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            if (response.IsSuccessful)
+            {
+                if (!string.IsNullOrEmpty(response.Content))
+                {
+
+                    Token tok = JsonConvert.DeserializeObject<Token>(response.Content);
+                    responseObj = tok.AccessToken;
+                    txtToken.Text = responseObj;
+                }
+            }
+
+
+            return responseObj;
+        }
+
+        public void GetInfo(string authorizeToken)
+        {
+            // Initialization.  
+            string responseObj = string.Empty;
+
+            // HTTP GET.  
+            var client = new RestClient(txturl.Text + "/api/values");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("postman-token", "f58802e1-3c35-9867-5833-7c33f0a46732");
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("authorization", "Bearer " + authorizeToken);
+            IRestResponse response = client.Execute(request);
+            if (response.IsSuccessful)
+            {
+                txtResponse.Text = response.Content;
+            }
+        }
+    }
+
+    internal class Token
+    {
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("token_type")]
+        public string TokenType { get; set; }
+
+        [JsonProperty("expires_in")]
+        public int ExpiresIn { get; set; }
+
+        [JsonProperty("refresh_token")]
+        public string RefreshToken { get; set; }
+    }
+}
